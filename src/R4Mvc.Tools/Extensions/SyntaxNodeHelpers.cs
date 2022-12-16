9d923a769de1b454e1be64f435ab43ff8f57bec1
daniel-green-nes <daniel.green@nes.scot.nhs.uk>
2022-12-16 23:06:16 +0000

    Tidy

--- src/R4Mvc.Tools/Extensions/SyntaxNodeHelpers.cs
+++ src/R4Mvc.Tools/Extensions/SyntaxNodeHelpers.cs
@@ -206,15 +206,11 @@ namespace R4Mvc.Tools.Extensions
             // This accounts for the "default namespace" case
             string nameSpace = string.Empty;
 
-            // Get the containing syntax node for the type declaration
-            // (could be a nested type, for example)
+            // Get the containing syntax node for the type declaration (could be a nested type, for example)
             SyntaxNode potentialNamespaceParent = syntax.Parent;
 
-            // Keep moving "out" of nested classes etc until we get to a namespace
-            // or until we run out of parents
-            while (potentialNamespaceParent != null &&
-                    potentialNamespaceParent is not NamespaceDeclarationSyntax
-                    && potentialNamespaceParent is not FileScopedNamespaceDeclarationSyntax)
+            // Keep moving "out" of nested classes etc until we get to a namespace or until we run out of parents
+            while (potentialNamespaceParent is not (NamespaceDeclarationSyntax or FileScopedNamespaceDeclarationSyntax or null))
             {
                 potentialNamespaceParent = potentialNamespaceParent.Parent;
             }
@@ -225,8 +221,7 @@ namespace R4Mvc.Tools.Extensions
                 // We have a namespace. Use that as the type
                 nameSpace = namespaceParent.Name.ToString();
 
-                // Keep moving "out" of the namespace declarations until we 
-                // run out of nested namespace declarations
+                // Keep moving "out" of the namespace declarations until we run out of nested namespace declarations
                 while (true)
                 {
                     if (namespaceParent.Parent is not NamespaceDeclarationSyntax parent)
